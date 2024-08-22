e9f8fe9ad869e7471c2028131a0ef8830b51e446
Acevedo Pablo Sebastian <pabloz21v@gmail.com>
2024-08-22 18:34:52 -0300

    limpieza de codigo

--- js/fetch.js
+++ js/fetch.js
@@ -1,29 +1,39 @@
 
 //CARGA INICIAL
 document.addEventListener('DOMContentLoaded', () => {
-    let busqueda = 'electrodomestico'
+    let busqueda = 'Electrodomesticos'
+    document.querySelector('#busqueda').innerHTML = "Resultados de: "+busqueda;
     fetchFuncion(busqueda)
+    carrusel()
 });
+
+//Fecht carga inicial para el carrusel
+function carrusel(){
+    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=` + busqueda+ `&limit=5#json`)
+    .then(response => response.json())
+    .catch(error => dibujarError(error))
+}
 
 //MENU 
 function getGet(element, value) {
     console.log(element);
+    document.querySelector('#busqueda').innerHTML = value;
     fetchFuncion(value)
 }
 
 
-//FECHT FUNCION
+//FECHT de los botones del menu, entrega 5 resultados
 function fetchFuncion(busqueda) {
-    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=` + busqueda + `&limit=5#json`)
+    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=` + busqueda+ `&limit=5#json`)
         .then(response => response.json())
         .then(response => renderProducts(response))
         .catch(error => dibujarError(error))
 }
 
-// BUSCADOR
+// BUSCADOR que entrega 10 resultados
 function getProducts() {
     let buscadorML = document.getElementById('buscadorML').value
-
+    document.querySelector('#busqueda').innerHTML = "Resultados de: "+buscadorML;
     const respuesta = fetch(`https://api.mercadolibre.com/sites/MLA/search?q=` + buscadorML + `&limit=10#json`);
     respuesta
         .then(response => response.json())
@@ -31,6 +41,7 @@ function getProducts() {
         .catch(error => dibujarError(error))
 }
 
+// Renderizado tabla de productos
 function renderProducts(response) {
     const products = response.results;
     let rows = '';
@@ -38,54 +49,18 @@ function renderProducts(response) {
         rows += `
         <tr>
             <td>${product.title}</td>
-            <td>
-                <img src="${product.thumbnail}" alt="" class="img-fluid">
+            <td >
+                <img src="${product.thumbnail}" alt="" class="img-thumbnail" style="max-width: 100px;">
             </td>
-            <td><button type="button" onclick="openModalProduct(${product.id})" id="${product.id}" class="btn btn-primary" 
-            data-bs-toggle="modal" data-bs-target="#modalResultadoBusqueda" >Mas info</button></td>
-            </tr>
+        </tr>
         `
     }
     document.querySelector('#products').innerHTML = rows;
 }
 
-// fetch por producto para modal
-function openModalProduct(product) {
-    const respuesta = fetch(`https://api.mercadolibre.com/items/` + product.id);
-    respuesta
-        .then(response => response.json())
-        .then(response => renderModalProduct(response))
-        .catch(error => dibujarError(error))
-}
-
-// contenido modal
-function renderModalProduct(response) {
-    const product = response;
-    let modalResultadoBusqueda =
-        `
-        <div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title">${product.title}</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-			</div>
-			<div class="modal-body d-flex justify-content-center">
-                <div class="card mb-3">
-                <img src="${product.thumbnail}" class="card-img-top" alt="${product.title}">
-                <div class="card-body">
-                    <h5 class="card-title">${product.currency_id} ${product.price}</h5>
-                    <p class="card-text">${product.warranty}</small></p>
-                </div>
-			</div>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-            <button type="button" class="btn btn-primary">Comprar</button>
-        </div>
-        `
-    document.querySelector('#datosResultadoBusqueda').innerHTML = modalResultadoBusqueda;
-}
 
 function dibujarError(error) {
+    document.querySelector('#busqueda').innerHTML = "Busqueda no encontrada";
     console.log(error);
     console.table(error);
 }
